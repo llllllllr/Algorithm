bf0b89bbb3f32387b904ae881a13e0399d3c9ce6
llllllllr <35605494+llllllllr@users.noreply.github.com>
2019-03-09 13:07:36 +0800

    排序完善

---
+++ 排序算法图解总结归纳/quickSort.cpp
@@ -0,0 +1,51 @@
+﻿
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+
+void	quickSort(int data[], int left, int right)
+{
+	if (left >= right)
+		return;
+
+	int i = left, j = right;
+	int key = data[left];
+	while (i < j)
+	{
+		while (i<j && data[j]>key)
+			j--;
+		if(i<j)
+		data[i++] = data[j];
+		while (i < j && data[i] < key)
+			i++;
+		if (i<j)
+		data[j--] = data[i];
+	}
+		data[i] = key;
+	
+	quickSort(data, left, i - 1);
+	quickSort(data, i + 1, right);
+
+}
+
+void show(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << a[i] << "  ";
+	cout << endl;
+}
+int main()
+{
+
+	int n;
+	cin >> n;
+	int* data = (int*)malloc(sizeof(int)*n);
+	for (int i = 0; i < n; i++)
+		cin >> data[i];
+	quickSort(data, 0, n - 1);
+	show(data, n);
+    return 0;
+}
+
